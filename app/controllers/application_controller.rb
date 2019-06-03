76dcc7296e26b93bb04a6230908779f9cdc19a9c
Francisco Iglesias <franciscoj.iglesias@hotmail.com>
2019-06-03 20:51:06 -0300

    Added migration and user adding possible logic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,8 @@ class ApplicationController < ActionController::Base
 
   # i18n configuration. See: http://guides.rubyonrails.org/i18n.html
   before_action :set_locale
+  auth = { username: 'francisco.iglesias@wolox.com.ar', password: '<insert-token-here>' }
+  headers = {'Accept' => 'application/json', 'Content-Type' => 'application/json'}
 
   def set_locale
     I18n.locale = params[:locale] || I18n.default_locale
@@ -22,8 +24,6 @@ class ApplicationController < ActionController::Base
   def index; end
 
   def first_version_create
-    # auth = { username: 'francisco.iglesias@wolox.com.ar', password: '<insert-token-here>' }
-    # headers = {'Accept' => 'application/json', 'Content-Type' => 'application/json'}
     # body = {
     #   description: 'Example Project description',
     #   leadAccountId: '5b5f4c49665a402d2a5a1c7d',
@@ -36,5 +36,36 @@ class ApplicationController < ActionController::Base
     #   key: 'EX'
     # }
     # HTTParty.post('https://wolox-support.atlassian.net/rest/api/2/project', basic_auth: auth, body: body, headers: headers)
+  end
+
+  def migrate_cards
+    # file = File.open 'maquetado.json'
+    # data = JSON.parse file
+    # file.close
+    # body = {
+    #   issueUpdates: data['cards'].map do |card|
+    #   return {
+    #     update: {},
+    #     fields: {
+    #       project: {
+    #         id: '<project_id>'
+    #       },
+    #       summary: card['name'],
+    #       description: card['desc']
+    #     }
+    #   }
+    # end
+    # }
+    # HTTParty.post('https://wolox-support.atlassian.net/rest/api/2/issue/bulk', basic_auth: auth, body: body, headers: headers)
+  end
+
+  def add_users_to_project
+    # body = {
+    #   user: [
+    #     '<trainer_user_id>'
+    #     '<trainee_user_id>'
+    #   ]
+    # }
+    # HTTParty.post("https://wolox-support.atlassian.net//rest/api/3/project/#{project_id}/role/#{role_id}", basic_auth: auth, body: body, headers: headers)
   end
 end
