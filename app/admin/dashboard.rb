23e15c48780b9eb520924a512b082c2b45fe1526
mtejedor <manuel.tejedor@wolox.com.ar>
2019-06-14 19:43:49 -0300

    removed commented code

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -1,10 +1,6 @@
 ActiveAdmin.register_page 'Dashboard' do
   menu priority: 1, label: proc { I18n.t('active_admin.dashboard') }
 
-  # action_item :new, only: :index do
-  #   link_to('Create Training', admin_trainings_new_path) # TODO: add I18n
-  # end
-
   content title: proc { I18n.t('active_admin.dashboard') } do
     div class: 'blank_slate_container', id: 'dashboard_default_message' do
       span class: 'blank_slate' do
