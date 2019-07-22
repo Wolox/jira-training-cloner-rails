8b41b1a8fa0467319644b00677d92990f2a55ae6
Francisco Iglesias <franciscoj.iglesias@hotmail.com>
2019-07-22 16:25:30 -0300

    Added new training

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,4 @@ Tech.create!(name: 'Maquetado', url: 'https://firebasestorage.googleapis.com/v0/
 Tech.create!(name: 'Android', url: 'https://firebasestorage.googleapis.com/v0/b/jira-training-cloner.appspot.com/o/android.json?alt=media&token=c5f1dcf6-964b-4dec-96d8-dfebf8c8ae97')
 Tech.create!(name: 'Node', url: 'https://firebasestorage.googleapis.com/v0/b/jira-training-cloner.appspot.com/o/node.json?alt=media&token=e5893a5d-fcff-449a-a1f1-c195709bc70e')
 Tech.create!(name: 'React Native', url: 'https://firebasestorage.googleapis.com/v0/b/jira-training-cloner.appspot.com/o/react-native.json?alt=media&token=554c7eac-31e3-41d9-adf1-889709cb6296')
+Tech.create!(name: 'Infraestructure', url:'https://firebasestorage.googleapis.com/v0/b/jira-training-cloner.appspot.com/o/infraestructure.json?alt=media&token=0ffaf7bf-5c6d-43fc-9a42-f42fcc30803a')
