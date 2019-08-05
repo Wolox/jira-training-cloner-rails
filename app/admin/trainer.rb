3ff6dc3019204fe5d4f630dd3a01549ef9f84f96
Francisco Iglesias <franciscoj.iglesias@hotmail.com>
2019-08-05 14:51:23 -0300

    Added bool_column and deleted required from input

--- app/admin/trainer.rb
+++ app/admin/trainer.rb
@@ -7,8 +7,8 @@ ActiveAdmin.register Trainer do
     column :name
     column :email
     column :trainer_teches { |trainer| trainer.trainer_teches.collect {|trainer_tech| trainer_tech.tech.name}.join(', ') }
-    column :trainer_to_be { |trainer| trainer.trainer_teches.any? { :trainer_to_be? } ? 'Yes' : 'No' }
-    column :active { |trainer| trainer.trainer_teches.any? { :active? } ? 'Yes' : 'No' }
+    bool_column :trainer_to_be { |trainer| trainer.trainer_teches.any? { :trainer_to_be? } }
+    bool_column :active { |trainer| trainer.trainer_teches.any? { :active? } }
     actions
   end
 
@@ -20,8 +20,8 @@ ActiveAdmin.register Trainer do
       f.input :name
       f.has_many :trainer_teches, allow_destroy: true do |tt| # TODO: change label
         tt.input :tech, as: :select, collection: Tech.all # TODO: change label
-        tt.input :active, required: false, as: :boolean
-        tt.input :trainer_to_be, required: false, as: :boolean
+        tt.input :active, as: :boolean
+        tt.input :trainer_to_be, as: :boolean
       end
     end
     f.actions
